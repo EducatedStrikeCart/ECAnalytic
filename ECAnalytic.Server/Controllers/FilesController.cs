ff9007b2e530e6782439ef5283fdc4489bcd015e
EducatedStrikeCart <dhn004@gmail.com>
2025-07-07 21:40:37 -0400

    add basic file upload backend

---
+++ ECAnalytic.Server/Controllers/FilesController.cs
@@ -0,0 +1,70 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Mime;
+using static Microsoft.EntityFrameworkCore.Query.Internal.ExpressionTreeFuncletizer;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ECAnalytic.Server.Controllers
+{
+
+
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FilesController : ControllerBase
+	{
+		private IConfiguration _config;
+		private ILogger<FilesController> _logger;
+		//public IFormFile DataCsv { get; set; }
+
+		public FilesController(IConfiguration config, ILogger<FilesController> logger)
+		{
+			_config = config;
+			_logger = logger;
+		}
+
+		// POST api/<ValuesController>
+		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status415UnsupportedMediaType)]
+
+		public async Task<IActionResult> Post([FromForm] List<IFormFile> files)
+		{
+
+			string? requestHeader = Request.ContentType ?? String.Empty;
+			string contentType = requestHeader.Split(';')[0]; // Remove multiform boundary indicator
+			if (contentType != "multipart/form-data")
+			{
+				return new UnsupportedMediaTypeResult();
+			}
+
+			string mysqlSecureFilePrivPath = _config["MySQLSecureFileDir"] ?? throw new Exception("MySQL secure file directory not configured");
+			string path = Path.GetFullPath(mysqlSecureFilePrivPath);
+			// [Do not use user supplied filename](https://learn.microsoft.com/en-us/aspnet/core/mvc/models/file-uploads?view=aspnetcore-9.0#security-considerations)
+			string fileName = Path.Combine(mysqlSecureFilePrivPath, Guid.NewGuid() + ".csv");
+
+			if (files.Count > 0)
+			{
+				using (var stream = System.IO.File.Create(fileName))
+				{
+					foreach (IFormFile file in files)
+						try
+						{
+							await file.CopyToAsync(stream);
+						}
+						catch (Exception e)
+						{
+							_logger.LogWarning(e.Message);
+						}
+				}
+				_logger.LogInformation("File creation finished");
+				return Ok();
+			}
+			return new BadRequestResult();
+		}
+	}
+}
---
+++ ECAnalytic.Server/Data/DbContext.cs
@@ -0,0 +1,29 @@
+﻿using ECAnalytic.Server.Controllers;
+using ECAnalytic.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Pomelo.EntityFrameworkCore.MySql;
+
+namespace ECAnalytic.Server.Data
+{
+	public class ECADbContext : DbContext
+	{
+		private readonly IConfiguration _config;
+		public ECADbContext(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var serverVersion = new MySqlServerVersion(new Version(8, 0, 42));
+			var connectionString = _config["ConnectionString"] ?? throw new Exception("Connection string not set up in User Secrets");
+			optionsBuilder.UseMySql(
+				connectionString,
+				serverVersion,
+				options => options.EnableRetryOnFailure());
+		}
+
+		public DbSet<Order> Orders { get; set; }
+	}
+}
