db6302877a0a495f772fd0872ff8a07722ebd2ce
EducatedStrikeCart <dhn004@gmail.com>
2025-08-07 22:47:04 -0400

    update solution and program.cs for Order controller, etc.

--- ECAnalytic.Server/Program.cs
+++ ECAnalytic.Server/Program.cs
@@ -1,11 +1,33 @@
+using ECAnalytic.Server.Data.Repositories;
+using ECAnalytic.Server.Infrastructure.Contracts;
+using ECAnalytic.Server.Infrastructure.Entities;
+using ECAnalytic.Server.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+	options.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("DefaultPolicy",
+		policy =>
+		{
+			policy.WithOrigins("https://localhost:55555")
+					.AllowAnyMethod()
+					.AllowAnyHeader();
+		});
+});
+builder.Services.AddProblemDetails();
+builder.Services.AddTransient<IFileService, FileService>();
+builder.Services.AddTransient<IBaseEntityFrameworkRepository<Order, OrderDto, Guid>,
+								BaseEntityFrameworkRepository<Order, OrderDto, Guid>>();
 
 var app = builder.Build();
 
@@ -15,13 +37,17 @@ app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthorization();
+
+app.UseExceptionHandler();
 
 app.MapControllers();
 
