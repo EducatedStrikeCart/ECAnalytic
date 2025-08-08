611ca2e38607d6c2c3113bf62943830351e9a54f
EducatedStrikeCart <dhn004@gmail.com>
2025-08-07 22:47:51 -0400

    add ability to upload files

--- ECAnalytic.Server/Controllers/FilesController.cs
+++ ECAnalytic.Server/Controllers/FilesController.cs
@@ -1,70 +1,40 @@
-﻿using Humanizer;
-using Microsoft.AspNetCore.Http;
+﻿using ECAnalytic.Server.Infrastructure.Contracts;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Formatters;
-using Microsoft.Extensions.Logging;
-using System.Net;
-using System.Net.Mime;
-using static Microsoft.EntityFrameworkCore.Query.Internal.ExpressionTreeFuncletizer;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ECAnalytic.Server.Controllers
 {
-
-
-	[Route("api/[controller]")]
 	[ApiController]
-	public class FilesController : ControllerBase
+	[Route("api/[controller]")]
+	[EnableCors("DefaultPolicy")]
+	public class FilesController(IConfiguration config, ILogger<FilesController> logger, IFileService fileService) : ControllerBase
 	{
-		private IConfiguration _config;
-		private ILogger<FilesController> _logger;
-		//public IFormFile DataCsv { get; set; }
-
-		public FilesController(IConfiguration config, ILogger<FilesController> logger)
+		[HttpGet]
+		public IActionResult Get()
 		{
-			_config = config;
-			_logger = logger;
+			return new BadRequestResult();
 		}
 
-		// POST api/<ValuesController>
 		[HttpPost]
-		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
 		[ProducesResponseType(StatusCodes.Status415UnsupportedMediaType)]
 
-		public async Task<IActionResult> Post([FromForm] List<IFormFile> files)
+		public async Task<ActionResult> Post(IFormFileCollection files)
 		{
-
 			string? requestHeader = Request.ContentType ?? String.Empty;
 			string contentType = requestHeader.Split(';')[0]; // Remove multiform boundary indicator
-			if (contentType != "multipart/form-data")
-			{
-				return new UnsupportedMediaTypeResult();
-			}
+			if (contentType != "multipart/form-data") return new UnsupportedMediaTypeResult();
 
-			string mysqlSecureFilePrivPath = _config["MySQLSecureFileDir"] ?? throw new Exception("MySQL secure file directory not configured");
-			string path = Path.GetFullPath(mysqlSecureFilePrivPath);
+			string mysqlSecureFilePrivPath = config["MySQLSecureFileDir"] ?? throw new Exception("MySQL secure file directory not configured");
+			
 			// [Do not use user supplied filename](https://learn.microsoft.com/en-us/aspnet/core/mvc/models/file-uploads?view=aspnetcore-9.0#security-considerations)
-			string fileName = Path.Combine(mysqlSecureFilePrivPath, Guid.NewGuid() + ".csv");
-
-			if (files.Count > 0)
-			{
-				using (var stream = System.IO.File.Create(fileName))
-				{
-					foreach (IFormFile file in files)
-						try
-						{
-							await file.CopyToAsync(stream);
-						}
-						catch (Exception e)
-						{
-							_logger.LogWarning(e.Message);
-						}
-				}
-				_logger.LogInformation("File creation finished");
-				return Ok();
-			}
-			return new BadRequestResult();
+			string fileNameWithPath = Path.Combine(mysqlSecureFilePrivPath, Guid.NewGuid() + ".csv");
+			
+			await fileService.SaveToDirAsync(files, fileNameWithPath);
+			
+			return NoContent();
 		}
 	}
 }
--- ecanalytic.client/src/pages/FileLoader.tsx
+++ ecanalytic.client/src/pages/FileLoader.tsx
@@ -2,38 +2,93 @@ import axios from "axios";
 import { useState, type ChangeEvent } from "react";
 
 const FileLoader = () => {
-    const [file, setFile] = useState<File>();
+    const [uploadedFiles, setUploadedFiles] = useState<FileList | null>(null);
+    const [files, setFiles] = useState<File[]>([]);
 
-    const handleSubmit = () => {
+    const handleSubmit: React.FormEventHandler<HTMLFormElement> = (
+        event: React.FormEvent<HTMLFormElement>
+    ) => {
+        event.preventDefault();
         const formData = new FormData();
-        if (file) {
-            formData.append("file", file);
-            formData.append("fileName", file.name);
-            axios.post("/Files", formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
+
+        // Split large files into smaller files
+        if (uploadedFiles && uploadedFiles.length > 0) {
+            preprocessFiles(uploadedFiles).forEach((file) => {
+                formData.append("file", file);
+                // formData.append("fileName", file.name);
             });
         }
+        console.log(formData);
+        for (const pair of formData.entries()) {
+            console.log(pair[0] + ", " + pair[1]);
+        }
+        axios.post("https://localhost:7170/api/Files", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
     };
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files;
+        setUploadedFiles(files);
+    };
+
+    const preprocessFiles = (files: FileList) => {
+        const filesArray: File[] = [];
+
         if (files) {
-            const selectedFile = files[0];
-            setFile(selectedFile);
+            for (let i = 0; i < files.length; i++) {
+                const file = files[i];
+                if (file.size > 30_000_000) {
+                    splitBigFiles(file).forEach((e: File) =>
+                        filesArray.push(e)
+                    );
+                } else {
+                    filesArray.push(file);
+                }
+            }
         }
+
+        return filesArray;
+    };
+
+    const splitBigFiles = (file: File, maxFileSize = 20_000_000): File[] => {
+        if (file.size < maxFileSize) return [file];
+
+        const filesArray: File[] = [];
+        const chunkSize = maxFileSize;
+        const chunksTotal = Math.ceil(file.size / maxFileSize);
+        let chunkNumber = 0;
+
+        while (chunkNumber < chunksTotal) {
+            const offset = chunkNumber * chunkSize;
+            const fileFromBlob = new File(
+                [file.slice(offset, chunkSize + offset)],
+                "chunk" + chunkNumber
+            );
+            filesArray.push(fileFromBlob);
+            chunkNumber++;
+        }
+
+        return filesArray;
+    };
+
+    const [test, setTest] = useState<string>("init");
+    const handleTest = () => {
+        axios.get("https://localhost:7170/api/Files").then((response) => {
+            console.log(response.data);
+        });
     };
 
     return (
         <div>
-            <form>
+            {test}
+            <form onSubmit={handleSubmit}>
                 <input type="file" onChange={handleChange} />
-                <button type="submit" onSubmit={handleSubmit}>
-                    Upload
-                </button>
+                <button type="submit">Upload</button>
             </form>
-           {file?.name}
+            <button onClick={handleTest}>TEST</button>
         </div>
     );
 };
